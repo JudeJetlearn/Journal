787d5caa830ca72446b3659198591d2b8197ed5c
JudeJetlearn <joefem5@gmail.com>
2026-07-19 16:01:27 +0100

    Add files via upload

---
+++ Class4/Main.py
@@ -0,0 +1,23 @@
+journal = r"C:\Users\judea\OneDrive\Desktop\Python V2\Class4\Journal.txt"
+
+while True:
+
+    print("1) View File ")
+    print("2) Add to File ")
+    print("3) Remove Info on File ")
+
+    option = int(input("Pick An Option. "))
+
+    if option == 1 :
+        with open(journal,"r")as readFile :
+            txt = readFile.read()
+            print(txt)
+
+    if option == 2 :
+        with open(journal,"a")as writeFile :
+            text = input("Type the Info to Add. ")
+            writeFile.write("\n"+text)
+    
+    if option == 3 :
+        with open(journal,"w")as RFile :
+            RFile.write("")
